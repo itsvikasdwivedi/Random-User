e1806d2d39a35da0c317e1df26253b2787304c5a
itsvikasdwivedi <vickyserver22@gmail.com>
2023-02-20 08:46:52 +0530

    backend

---
+++ server/database/db.js
@@ -0,0 +1,16 @@
+import mongoose  from "mongoose"
+import dotenv from "dotenv"
+dotenv.config();
+export default  mongoose.connect(process.env.MONGOURL)
+
+mongoose.connection.on("connected",()=>{
+    console.log("connected to database successfully,");
+})
+
+mongoose.connection.on("error",(err)=>{
+    console.log("Error while connection to database:" + err );
+})
+
+mongoose.connection.on("disconnected",(err)=>{
+    console.log("Mongodb connection disconnected");
+})
---
+++ server/index.js
@@ -0,0 +1,24 @@
+
+import express from "express"
+import cors  from "cors"
+import  Connection  from "./database/db.js"
+import userRouter from "./routes/users.js"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use("/user", userRouter)
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Random User API")
+})
+
+app.listen( process.env.PORT || 8080, async()=>{
+    await Connection
+    console.log("server started at http://localhost:8080");
+})
+
